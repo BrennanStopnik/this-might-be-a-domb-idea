4e4a6408b2fd49fea912f81c3c2c4aa7b2942fa7
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-06-28 11:43:25 -0400

    made some changes

---
+++ main.js
@@ -0,0 +1,29 @@
+let para = document.querySelector('p');
+para.style.color = 'lightblue';
+
+
+let header = document.querySelector('h1');
+header.style.fontSize = '48px';
+
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = ('I say, "Hi!"');
+
+let image1 = document.querySelector('#image-1');
+image1.src = ('https://project-orion-production.s3.amazonaws.com/uploads/content/21499/Jul20_FW-RamDass.jpg');
+image1.style.height = '300px';
+
+let image2 = document.querySelector('#image-2');
+image2.src = ('https://i1.sndcdn.com/artworks-000044411644-o56o6t-t500x500.jpg');
+image2.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = ("Won't get fooled again.");
+
+let body = document.querySelector('body');
+body.style.background = ('goldenrod')
